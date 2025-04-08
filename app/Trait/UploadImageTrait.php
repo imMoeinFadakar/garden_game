aaf92d190550b694fd08c96557d41e9db5bc456f
ImMoeinFadakar <moeinfadakar3@gmail.com>
2025-04-08 03:22:05 -0700

    init - init project

--- app/Trait/UploadImageTrait.php
+++ app/Trait/UploadImageTrait.php
@@ -12,7 +12,7 @@ trait UploadImageTrait
     public function uploadImage($request,$dirName)
     {
        $image =  $request->file("image_url"); // find its image 
-        $image_name = rand(10,1000) .Carbon::now()->microsecond.$image->extension();
+        $image_name = rand(10,1000) .Carbon::now()->microsecond.'.'.$image->extension();
         $path  = $image->storeAs('images/'.$dirName , $image_name,); // store in storage and return path
         return   Storage::url($path);  // return full path
     }
